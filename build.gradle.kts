7250021cd40877ac7d6a21260a27ec4ceea516e8
Blaz Vantur <blaz.vantur@gmail.com>
2024-09-24 12:18:12 +0200

    feat: add detekt to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
@@ -9,10 +12,12 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.detekt) apply true
 }
 
 subprojects {
     ktlintSetup()
+    detektSetup()
 }
 
 fun Project.ktlintSetup() {
@@ -24,5 +29,52 @@ fun Project.ktlintSetup() {
         filter {
             exclude { it.file.path.contains("build/") }
         }
+    }
+}
+
+fun Project.detektSetup() {
+    apply(plugin = rootProject.libs.plugins.detekt.get().pluginId)
+
+    dependencies {
+        detektPlugins(rootProject.libs.detekt.formatting)
+    }
+
+    detekt {
+        buildUponDefaultConfig = true
+        allRules = false
+        source.from(
+            "src/androidMain/kotlin",
+            "src/commonMain/kotlin",
+            "src/iosMain/kotlin"
+        )
+        config.setFrom("${rootProject.projectDir}/code-analysis-config/detekt/detekt.yml")
+        baseline = file("${rootProject.projectDir}/code-analysis-config/detekt/baseline.xml")
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        exclude("**/build/**", "**/generated/**", "**/resources/**")
+        autoCorrect = true
+
+        reports {
+            xml.required.set(false)
+            txt.required.set(false)
+            md.required.set(false)
+
+            html {
+                required.set(true)
+                outputLocation.set(
+                    layout.buildDirectory.file("reports/detekt.html")
+                )
+            }
+
+            sarif.required.set(true)
+        }
+    }
+
+    tasks.withType<Detekt>().configureEach {
+        jvmTarget = JvmTarget.JVM_17.target
+    }
+    tasks.withType<DetektCreateBaselineTask>().configureEach {
+        jvmTarget = JvmTarget.JVM_17.target
     }
 }
