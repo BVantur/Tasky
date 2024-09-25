8a54aa378a186f104a821ace9340516f1f0ddcd5
Blaz Vantur <blaz.vantur@gmail.com>
2024-09-25 16:13:04 +0200

    style: code analysis tools applied

--- composeApp/src/commonMain/kotlin/sp/bvantur/tasky/App.kt
+++ composeApp/src/commonMain/kotlin/sp/bvantur/tasky/App.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
