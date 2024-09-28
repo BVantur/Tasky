bfd08d00e7cf1cfbb12e35021d2fe363de362dfe
Blaz Vantur <blaz.vantur@gmail.com>
2024-09-28 11:16:04 +0200

    style: code analysis tools applied

--- composeApp/src/commonMain/kotlin/sp/bvantur/tasky/register/domain/RegisterUserUseCase.kt
+++ composeApp/src/commonMain/kotlin/sp/bvantur/tasky/register/domain/RegisterUserUseCase.kt
@@ -3,10 +3,9 @@ package sp.bvantur.tasky.register.domain
 import sp.bvantur.tasky.register.data.RegisterRepository
 
 class RegisterUserUseCase(private val repository: RegisterRepository) {
-    suspend operator fun invoke(name: String, email: String, password: String): Result<Unit> =
-        repository.register(
-            name = name,
-            email = email,
-            password = password
-        )
+    suspend operator fun invoke(name: String, email: String, password: String): Result<Unit> = repository.register(
+        name = name,
+        email = email,
+        password = password
+    )
 }
