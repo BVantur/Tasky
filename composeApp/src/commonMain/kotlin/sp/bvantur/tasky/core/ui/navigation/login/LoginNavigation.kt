d69e01e83dbf152a7abc9c363e1357a1cf4a1fbb
Blaz Vantur <blaz.vantur@gmail.com>
2024-10-02 11:10:11 +0200

    refactor: made constant for navigation route as internal

--- composeApp/src/commonMain/kotlin/sp/bvantur/tasky/core/ui/navigation/login/LoginNavigation.kt
+++ composeApp/src/commonMain/kotlin/sp/bvantur/tasky/core/ui/navigation/login/LoginNavigation.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import sp.bvantur.tasky.login.ui.LoginRoute
 
-const val LOGIN_NAVIGATION_ROUTE = "login_navigation_route"
+internal const val LOGIN_NAVIGATION_ROUTE = "login_navigation_route"
 
 internal fun NavGraphBuilder.loginScreen(navigateToRegister: () -> Unit) {
     composable(
