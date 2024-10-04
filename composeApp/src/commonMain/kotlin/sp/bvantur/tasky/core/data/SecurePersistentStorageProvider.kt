c905deb73d68469c974f0203fe297008a74e2152
Blaz Vantur <blaz.vantur@gmail.com>
2024-10-04 14:00:57 +0200

    fix: failing Konsist tests

--- composeApp/src/commonMain/kotlin/sp/bvantur/tasky/core/data/SecurePersistentStorageProvider.kt
+++ composeApp/src/commonMain/kotlin/sp/bvantur/tasky/core/data/SecurePersistentStorageProvider.kt
@@ -3,10 +3,10 @@ package sp.bvantur.tasky.core.data
 import com.liftric.kvault.KVault
 
 abstract class SecurePersistentStorageProvider {
+    open lateinit var kVault: KVault
+
     companion object {
         const val ACCESS_TOKEN_KEY = "access_token"
         const val EXPIRATION_TIMESTAMP_TOKEN_KEY = "expiration_timestamp_token_key"
     }
-
-    open lateinit var kVault: KVault
 }
