76cf29e8aff5b88211738295362926e1fcd81952
Blaz Vantur <blaz.vantur@gmail.com>
2024-10-21 13:40:45 +0200

    style: code analysis changes applied

--- composeApp/src/commonMain/kotlin/sp/bvantur/tasky/event/data/mappers/EventMapper.kt
+++ composeApp/src/commonMain/kotlin/sp/bvantur/tasky/event/data/mappers/EventMapper.kt
@@ -5,14 +5,12 @@ import sp.bvantur.tasky.event.data.remote.CreateEventRequest
 import sp.bvantur.tasky.event.domain.model.Event
 import sp.bvantur.tasky.event.presentation.utils.extensions.getMillis
 
-fun Event.asCreateEventRequest(): CreateEventRequest {
-    return CreateEventRequest(
-        id = getTimeMillis().toString(), // TODO use UUID when update to Kotlin 2.0.20
-        title = title,
-        description = description,
-        from = fromTime.getMillis(),
-        to = toTime.getMillis(),
-        remindAt = reminder.inMillis,
-        attendeeIds = attendees.map { it.userId }
-    )
-}
+fun Event.asCreateEventRequest(): CreateEventRequest = CreateEventRequest(
+    id = getTimeMillis().toString(), // TODO use UUID when update to Kotlin 2.0.20
+    title = title,
+    description = description,
+    from = fromTime.getMillis(),
+    to = toTime.getMillis(),
+    remindAt = reminder.inMillis,
+    attendeeIds = attendees.map { it.userId }
+)
